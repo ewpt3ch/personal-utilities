f28e0a93d0e2927dcac6b739b6a92b4beb24622e
Eric Phillips <eric@ageekandhistech.com>
2010-10-03 18:37:32 -0600

    added variables for debugging and optional location of files

--- mkplaylist.sh
+++ mkplaylist.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-BASEDIR="/media/sdb1/"
+#usage mkplaylist.sh listname(optional) audiodir(optional)
+
+#variables
+BASEDIR="/media/sdb1"
+BASEDIR="/tmp/sdb1/" #uncomment to test or debug
 DATE=`date +%m%d`
-echo ${DATE}
-REALNAME="podcast-${DATE}"
-echo ${REALNAME}
+LISTNAME="podcast-${DATE}"
+AUDIODIR="${LISTNAME}"
 TMPFILE="/tmp/podcast-${DATE}.tmp"
+#print variables for debugging
+echo ${BASEDIR}
+echo ${DATE}
+echo ${LISTNAME}
+echo ${AUDIODIR}
 echo ${TMPFILE}
+
+#mk tmp playlist
 echo "PLP PLAYLIST" > ${TMPFILE}
 echo "VERSION 1.20" >>  ${TMPFILE}
 echo "" >> ${TMPFILE}
 cd "${BASEDIR}/MUSIC"
-for f in `find "${REALNAME}" -type f \( -name "*.[mM][pP]3" \)`
+for f in `find "${AUDIODIR}" -type f \( -name "*.[mM][pP]3" \)`
 do
     FILE=`echo ${f} | sed -e 's/\//\\\\/g'`
     echo "HARP, MUSIC\\${FILE}" >> ${TMPFILE}
 done
+
+# convert playlist for use on sansa e280 and copy it there
 unix2dos ${TMPFILE}
-iconv -f ASCII -t UTF16LE -o "${BASEDIR}PLAYLISTS/${REALNAME}.PLA" ${TMPFILE}
+iconv -f ASCII -t UTF16LE -o "${BASEDIR}PLAYLISTS/${LISTNAME}.PLA" ${TMPFILE}
 rm ${TMPFILE}
