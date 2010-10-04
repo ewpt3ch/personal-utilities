0e12529e7318f50cc09e5a6f493189e2ebebadde
Eric Phillips <eric@ageekandhistech.com>
2010-10-04 11:33:37 -0600

    added sort to mkplaylist

--- mkplaylist.sh
+++ mkplaylist.sh
@@ -36,18 +36,23 @@ then
     AUDIODIR=$2
 fi
 
-#mk tmp playlist
+#get file names for playlist and sort them
+
+cd "${BASEDIR}/MUSIC"
+find ${AUDIODIR} -type f -name "*.[mM][pP]3" | while read f
+do
+    FILE=`echo ${f} | sed -e 's/\//\\\\/g'`
+    echo ${FILE}
+    echo "HARP, MUSIC\\${FILE}" >> "${TMPFILE}unsorted"
+done
+
 echo "PLP PLAYLIST" > ${TMPFILE}
 echo "VERSION 1.20" >>  ${TMPFILE}
 echo "" >> ${TMPFILE}
-cd "${BASEDIR}/MUSIC"
-for f in `find "${AUDIODIR}" -type f \( -name "*.[mM][pP]3" \)`
-do
-    FILE=`echo ${f} | sed -e 's/\//\\\\/g'`
-    echo "HARP, MUSIC\\${FILE}" >> ${TMPFILE}
-done
+echo `sort "${TMPFILE}unsorted" >> ${TMPFILE}`
 
 # convert playlist for use on sansa e280 and copy it there
 unix2dos ${TMPFILE}
 iconv -f ASCII -t UTF16LE -o "${BASEDIR}/PLAYLISTS/${LISTNAME}.PLA" ${TMPFILE}
 rm ${TMPFILE}
+rm "${TMPFILE}unsorted"
